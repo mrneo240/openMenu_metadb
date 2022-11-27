3861d5d0f8dcdc2bf53074a8d71532d8dee335eb
Hayden Kowalchuk <819028+mrneo240@users.noreply.github.com>
2022-11-27 16:44:59 -0500

    fix: correct scripts

--- setup.sh
+++ setup.sh
@@ -7,11 +7,12 @@ TEMP=temp
 mkdir -p ${OUT}
 mkdir -p ${TOOLS}
 mkdir -p ${TEMP}
+mkdir -p ${meta_in}
 
-# clone tools and stbconv
+# clone tools
 git clone --depth 1 https://github.com/mrneo240/openmenu.git ${TEMP}/openmenu
 
-# build both
+# build tools
 make -j4 -C ${TEMP}/openmenu/tools
 
 # copy tools to tools dir
